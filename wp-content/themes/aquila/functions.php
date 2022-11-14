c4c14c298daf2876a2030693b3bebcecf445cc10
Arman Hossain <armanhossaincse7@gmail.com>
2022-11-14 21:57:55 +0600

    enqueue stylesheet

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -4,4 +4,9 @@
  * 
  * @package Aquila
  */
+
+ function aquila_enqueue_scripts(){
+   wp_enqueue_style( 'style', get_stylesheet_uri() , [], filemtime( get_template_directory(). '/style.css' ), 'all' );
+ }
+ add_action( 'wp_enqueue_scripts', 'aquila_enqueue_scripts' );
 ?>
