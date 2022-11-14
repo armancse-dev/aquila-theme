09988f17cc352d74246f4f4ddcc6e9b871515298
Arman Hossain <armanhossaincse7@gmail.com>
2022-11-14 22:12:01 +0600

    enqueue scripts

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -6,7 +6,15 @@
  */
 
  function aquila_enqueue_scripts(){
-   wp_enqueue_style( 'style', get_stylesheet_uri() , [], filemtime( get_template_directory(). '/style.css' ), 'all' );
+   // wp_enqueue_style( 'style', get_stylesheet_uri() , [], filemtime( get_template_directory(). '/style.css' ), 'all' );
+   // wp_enqueue_script( 'main-script', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory(). '/assets/main.js' ), true );
+
+   wp_register_style( 'style', get_stylesheet_uri() , [], filemtime( get_template_directory(). '/style.css' ), 'all' );
+   wp_register_script( 'main-script', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory(). '/assets/main.js' ), true );
+
+   wp_enqueue_style('style');
+   wp_enqueue_script('main-script');
+
  }
  add_action( 'wp_enqueue_scripts', 'aquila_enqueue_scripts' );
 ?>
--- wp-content/themes/aquila/header.php
+++ wp-content/themes/aquila/header.php
@@ -15,6 +15,7 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title><?php wp_title(); ?></title>
    <?php wp_head(); ?>
+   
 </head>
 <body <?php body_class(); ?> >
    <?php wp_body_open(); ?>
