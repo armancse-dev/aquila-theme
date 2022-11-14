a2bba7d7afc38d7ecae950abdd513481380762b4
Arman Hossain <armanhossaincse7@gmail.com>
2022-11-14 22:41:17 +0600

    get_template_part and bootstrap add

--- wp-content/themes/aquila/functions.php
+++ wp-content/themes/aquila/functions.php
@@ -9,11 +9,28 @@
    // wp_enqueue_style( 'style', get_stylesheet_uri() , [], filemtime( get_template_directory(). '/style.css' ), 'all' );
    // wp_enqueue_script( 'main-script', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory(). '/assets/main.js' ), true );
 
+   //register styles
    wp_register_style( 'style', get_stylesheet_uri() , [], filemtime( get_template_directory(). '/style.css' ), 'all' );
+
+   //register Bootstrap
+   wp_register_style( 'bootstrap-css', get_template_directory_uri(). '/assets/src/library/css/bootstrap.min.css' , [], false, 'all' );
+
+   //register scripts
    wp_register_script( 'main-script', get_template_directory_uri() . '/assets/main.js', [], filemtime( get_template_directory(). '/assets/main.js' ), true );
 
+   //register bootstrap scripts
+   wp_register_script( 'bootstrap-js', get_template_directory_uri() . '/assets/src/library/js/bootstrap.min.js', ['jquery'], true );
+
+   //enqueue style
    wp_enqueue_style('style');
+   wp_enqueue_style('bootstrap-css');
+
+
+   //enqueue scripts
    wp_enqueue_script('main-script');
+   wp_enqueue_script('bootstrap-js');
+
+   
 
  }
  add_action( 'wp_enqueue_scripts', 'aquila_enqueue_scripts' );
--- wp-content/themes/aquila/header.php
+++ wp-content/themes/aquila/header.php
@@ -15,10 +15,14 @@
    <meta name="viewport" content="width=device-width, initial-scale=1.0">
    <title><?php wp_title(); ?></title>
    <?php wp_head(); ?>
-   
+
 </head>
 <body <?php body_class(); ?> >
-   <?php wp_body_open(); ?>
-   <header>
-      <h2>header</h2>
-   </header>
+<?php 
+   if (function_exists( 'wp_body_open' ) ){
+      wp_body_open();
+   }
+?>
+<header>
+   <h2>header</h2>
+</header>
